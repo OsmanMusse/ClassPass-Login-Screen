fed04978ee3fb882c6a0e3b28b4a98f87c988a6c
Mezut <you@example.com>
2019-06-25 00:26:08 +0100

    pushing all files

---
+++ StackViews/ViewController.swift
@@ -0,0 +1,170 @@
+//
+//  ViewController.swift
+//  StackViews
+//
+//  Created by Mezut on 23/06/2019.
+//  Copyright © 2019 Mezut. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+    
+    var facebookbtn: UIButton = {
+       let button = UIButton()
+        button.setTitle("Log in with Facebook", for: .normal)
+        button.backgroundColor = UIColor(red: 65/255, green: 103/255, blue: 178/255, alpha: 1)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.layer.cornerRadius = 3
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    var facebookView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var facebookLogo: UIImageView = {
+       let image = UIImageView(image: #imageLiteral(resourceName: "facebook-logo"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    
+  
+     var optionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "or"
+        label.textColor = UIColor(red: 139/255, green: 139/255, blue: 139/255, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    var emailTextField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Email address"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    
+    var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var loginBtn: UIButton =  {
+       let button = UIButton()
+        button.setTitle("Log in", for: .normal)
+        button.setTitleColor( UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1), for: .normal)
+        button.backgroundColor = UIColor(red: 228/255, green: 228/255, blue: 228/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var forgotLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Forget your password?"
+        label.textColor = UIColor(red: 139/255, green: 139/255, blue: 139/255, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    var stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        stackView.addArrangedSubview(optionLabel)
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(loginBtn)
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        stackView.distribution = .fillEqually
+        setupDesign()
+        setupViews()
+        
+        
+        
+        
+    }
+
+    
+    
+    
+    func setupDesign() {
+        view.addSubview(facebookbtn)
+        view.addSubview(facebookView)
+        view.addSubview(facebookLogo)
+    
+       
+         
+        
+        view.addSubview(stackView)
+        
+     view.addSubview(forgotLabel)
+     
+        
+        
+    }
+    
+    
+    func setupViews() {
+        NSLayoutConstraint.activate([
+              facebookbtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+              facebookbtn.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+              facebookbtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+              facebookbtn.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+              facebookbtn.heightAnchor.constraint(equalToConstant: 43),
+              
+              facebookView.leadingAnchor.constraint(equalTo: facebookbtn.leadingAnchor),
+              facebookView.trailingAnchor.constraint(equalTo: facebookbtn.trailingAnchor),
+              facebookView.topAnchor.constraint(equalTo: facebookbtn.topAnchor),
+              facebookView.heightAnchor.constraint(equalTo: facebookbtn.heightAnchor),
+              facebookView.centerYAnchor.constraint(equalTo: facebookbtn.centerYAnchor),
+            
+              facebookLogo.leadingAnchor.constraint(equalTo: facebookView.leadingAnchor, constant: 63),
+              facebookLogo.centerYAnchor.constraint(equalTo: facebookView.centerYAnchor),
+              facebookLogo.widthAnchor.constraint(equalToConstant: 20),
+              facebookLogo.heightAnchor.constraint(equalToConstant: 20),
+              
+              stackView.leadingAnchor.constraint(equalTo: facebookbtn.leadingAnchor),
+              stackView.trailingAnchor.constraint(equalTo: facebookbtn.trailingAnchor),
+              stackView.topAnchor.constraint(equalTo: facebookbtn.bottomAnchor, constant: 0),
+              stackView.heightAnchor.constraint(equalToConstant: 220),
+              
+              forgotLabel.topAnchor.constraint(equalTo: loginBtn.bottomAnchor, constant: 15),
+              forgotLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+              
+            
+            
+            ])
+    
+    }
+
+}
+
---
+++ StackViewsTests/StackViewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  StackViewsTests.swift
+//  StackViewsTests
+//
+//  Created by Mezut on 23/06/2019.
+//  Copyright © 2019 Mezut. All rights reserved.
+//
+
+import XCTest
+@testable import StackViews
+
+class StackViewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
